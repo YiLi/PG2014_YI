75fb2f08892ba21d18f0768161c0940193041acb
YiLi <li.yi.123@hotmail.com>
2014-12-14 16:09:36 -0600

    Update hw2p3.py

--- hw2/hw2p3.py
+++ hw2/hw2p3.py
@@ -1,24 +1,25 @@
 ##HW2Q3
 ##programmed by Yili
-##Oct 9th.
+##Dec 13th
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-def high_pass(Points,order=1):
+def high_pass(x,y,order=1):
 
-	x,y=Points[:,0],Points[:,0]
 
 	poly=np.polynomial.Polynomial.fit(x, y, order)
 	highpass=y-poly(x)
 
-	hppoints=[]
-	for i in range( len(x)):
-		hppoints.append([x[i],highpass[i]])
+	return highpass
 
-	return hppoints
+x=np.random.rand(100)
+y=np.random.rand(100)
 
-Points=np.random.rand(5,2)
+highpass=high_pass(x,y)
 
-hppoints=high_pass(Points)
+print highpass
 
-print hppoints
+plt.plot(x,y,'r.', label = 'Polynomial')
+plt.plot(x,highpass,'b.',label='Difference')
+plt.show()
